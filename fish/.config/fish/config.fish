ab9fee1c37ea90160dfe87b57a0f2ffbc398d0a4
sasquatchdev <about@sasquatchdev.me>
2025-03-23 15:03:33 +0100

    fix: fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,4 +9,18 @@ abbr -a -- nixr sudo nixos-rebuild --flake "$(readlink -f ~/.config/nixos)" swit
 starship init fish | source
 
 # adds the binaries from `cargo install` to path
-set -U fish_user_paths /home/sasquatchdev/.asdf/installs/rust/1.85.1/bin $fish_user_paths
+set -U fish_user_paths /home/sasquatchdev/.asdf/installs/rust/*/bin $fish_user_paths
+
+# adds asdf shims to path with fallback
+if set -q ASDF_DATA_DIR
+    set asdf_dir $ASDF_DATA_DIR
+else
+    set asdf_dir $HOME/.asdf
+end
+set -gx PATH $asdf_dir/shims $PATH
+
+# adds asdf completions
+asdf completion fish | source
+
+# evaluates the brew shellenv
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
