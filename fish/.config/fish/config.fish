a9d837b14d7752468ea6e40f6d87d6fcd4394e5f
sasquatchdev <about@sasquatchdev.me>
2025-03-23 14:49:37 +0100

    feat: update to starship config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,4 +5,8 @@ end
 abbr -a -- sude sudo -E
 abbr -a -- nixr sudo nixos-rebuild --flake "$(readlink -f ~/.config/nixos)" switch
 
+# sources the starship prompt
 starship init fish | source
+
+# adds the binaries from `cargo install` to path
+set -U fish_user_paths /home/sasquatchdev/.asdf/installs/rust/<version>/bin $fish_user_paths
